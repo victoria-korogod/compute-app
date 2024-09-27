5d2217dff70712afb7c664b02a08d395642ae07c
Victoria Korogod <victoria.k.biz@solid-sl.com>
2024-09-27 06:44:30 +0100

    Add constants with data

---
+++ apps/ui/src/pages/LandingPage/constant/index.ts
@@ -0,0 +1,121 @@
+export const navLinks = [
+  {
+    label: 'Home',
+    path: 'home',
+  },
+  {
+    label: 'Products',
+    path: 'products',
+  },
+  {
+    label: 'Enterprise',
+    path: 'enterprise',
+  },
+  {
+    label: 'Pricing',
+    path: 'pricing',
+  },
+  {
+    label: 'Resources',
+    path: 'resources',
+  },
+];
+
+export const tableCols = [
+  'GPU model',
+  'On-demand cost',
+  '1 month ',
+  '3 months',
+  '	6 months',
+  'Details',
+];
+
+export const tableRow = [
+  {
+    model: 'H100',
+    cost: 'from $2.79/hr',
+    month1: 'from $2.49/hr',
+    month3: 'from $2.30/hr',
+    month6: 'from $2.15/hr',
+    detail: 'More info',
+  },
+  {
+    model: 'V100',
+    cost: 'from $2.79/hr',
+    month1: 'from $2.49/hr',
+    month3: 'from $2.30/hr',
+    month6: 'from $2.15/hr',
+    detail: 'More info',
+  },
+  {
+    model: 'A40',
+    cost: 'from $2.79/hr',
+    month1: 'from $2.49/hr',
+    month3: 'from $2.30/hr',
+    month6: 'from $2.15/hr',
+    detail: 'More info',
+  },
+  {
+    model: 'L40S',
+    cost: 'from $2.79/hr',
+    month1: 'from $2.49/hr',
+    month3: 'from $2.30/hr',
+    month6: 'from $2.15/hr',
+    detail: 'More info',
+  },
+  {
+    model: 'RTXA5000',
+    cost: 'from $2.79/hr',
+    month1: 'from $2.49/hr',
+    month3: 'from $2.30/hr',
+    month6: 'from $2.15/hr',
+    detail: 'More info',
+  },
+  {
+    model: 'B100',
+    cost: 'from $2.79/hr',
+    month1: 'from $2.49/hr',
+    month3: 'from $2.30/hr',
+    month6: 'from $2.15/hr',
+    detail: 'More info',
+  },
+  {
+    model: 'RTXA4000',
+    cost: 'from $2.79/hr',
+    month1: 'from $2.49/hr',
+    month3: 'from $2.30/hr',
+    month6: 'from $2.15/hr',
+    detail: 'More info',
+  },
+  {
+    model: 'MI250/300',
+    cost: 'from $2.79/hr',
+    month1: 'from $2.49/hr',
+    month3: 'from $2.30/hr',
+    month6: 'from $2.15/hr',
+    detail: 'More info',
+  },
+];
+
+export const SliderData = [
+  {
+    name: 'Realtime monitoring',
+    review:
+      'Gain real-time insights into your GPU usage and resource allocation, allowing you to optimise your AI workflows. Track key performance indicators, identify bottlenecks and proactively manage your resources for maximum efficiency.',
+  },
+  {
+    name: 'Global network',
+    review:
+      'Access a geographically distributed network of high-performance GPUs in multiple regions. Leverage our extensive global infrastructure to model training and inference anywhere in the world.',
+  },
+  {
+    name: 'Cost management',
+    review:
+      'Benefit from transparent pricing models and detailed billing reports. Easily track your cloud resource expenditure and identify opportunities for cost optimisation.',
+  },
+  {
+    name: 'Commitment pricing',
+    review:
+      'Save up to 30% on GPU costs by committing to a fixed term. Our commitment pricing plans offer significant savings, ideal for long-term projects and workloads.',
+  },
+];
