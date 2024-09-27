f444522604214ff60f296cc213fe9d931364bba0
Victoria Korogod <victoria.k.biz@solid-sl.com>
2024-09-27 07:00:43 +0100

    Update landing page to use components

--- apps/ui/src/pages/LandingPage/LandingPage.tsx
+++ apps/ui/src/pages/LandingPage/LandingPage.tsx
@@ -1,5 +1,19 @@
+import BuildGpu from './components/BuildGpu'
+import Hero from './components/Hero'
+import ManageInfastructure from './components/ManageInfastructure'
+import SelectComponent from './components/SelectComponent'
+
 const LandingPage = () => {
-    return <div>LandingPage</div>
-  }
+  return (
+    <div>
+      <Hero />
+      <div className="container mx-auto">
+        <SelectComponent />
+        <BuildGpu />
+        <ManageInfastructure />
+      </div>
+    </div>
+  );
+}
   
 export default LandingPage
